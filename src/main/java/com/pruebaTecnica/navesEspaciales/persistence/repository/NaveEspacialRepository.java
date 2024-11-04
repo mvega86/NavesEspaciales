6b6db440cd9699d6c838196b3d1c085ee5b9c593
Magdiel <mvg140386@gmail.com>
2024-11-04 19:23:47 +0100

    Creado servicio y repositorio para la entidad NaveEspacial

---
+++ src/main/java/com/pruebaTecnica/navesEspaciales/persistence/repository/NaveEspacialRepository.java
@@ -0,0 +1,7 @@
+package com.pruebaTecnica.navesEspaciales.persistence.repository;
+
+import com.pruebaTecnica.navesEspaciales.persistence.entity.NaveEspacial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NaveEspacialRepository extends JpaRepository<NaveEspacial, Long>{
+}
