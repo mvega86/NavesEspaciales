5435324f1aa8d8fe97d78fe6e96c3b79140cc03a
Magdiel <mvg140386@gmail.com>
2024-11-05 16:02:00 +0100

    Añadiendo test de integración

--- src/test/java/com/pruebaTecnica/spaceShips/SpaceShipsApplicationTests.java
+++ src/test/java/com/pruebaTecnica/spaceShips/SpaceShipsApplicationTests.java
@@ -1,13 +1,29 @@
 package com.pruebaTecnica.spaceShips;
 
+import com.pruebaTecnica.spaceShips.persistence.entity.SpaceShip;
+import com.pruebaTecnica.spaceShips.persistence.repository.SpaceShipRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
-@SpringBootTest
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@Rollback(false) // Para que los cambios se mantengan durante el test
 class SpaceShipsApplicationTests {
 
+	@Autowired
+	private SpaceShipRepository spaceShipRepository;
+
 	@Test
-	void contextLoads() {
+	public void testDatabaseMigration() {
+		// Verifica que la tabla esté creada
+		List<SpaceShip> spaceShips = spaceShipRepository.findAll();
+		assertThat(spaceShips).isNotNull();
 	}
 
 }
